7bc85ecb079541453f5e224653171189a79d59fd
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2021-09-18 16:52:32 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-
+const app = new Vue({
+el: '#app',
+  data: {
+    startingValue: '',
+    yearsToRetirement: '',
+    monthlyContribution: '',
+    annualInterestRate: '',
+    desiredPortfolio: '',
+});
